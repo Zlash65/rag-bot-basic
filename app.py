a28ffd71b0c9b4bfd8e6157052b361836d16fee3
Zlash65 <zarrar65@gmail.com>
2025-07-03 19:51:25 +0530

    feat: add model provider selectbox & model options

--- app.py
+++ app.py
@@ -5,11 +5,31 @@ import streamlit as st
 from PyPDF2 import PdfReader
 
 
+# ------------------ Model Configuration ------------------ #
+MODEL_OPTIONS = {
+  "Groq": {
+    "playground": "https://console.groq.com/",
+    "models": ["llama-3.1-8b-instant", "llama3-70b-8192"]
+  },
+  "Gemini": {
+    "playground": "https://ai.google.dev",
+    "models": ["gemini-2.0-flash", "gemini-2.5-flash"]
+  }
+}
+
 # ------------------ Main App ------------------ #
 def main():
   st.set_page_config(page_title="RAG PDFBot", layout="centered")
   st.title("👽 RAG PDFBot")
   st.caption("Chat with multiple PDFs :books:")
+
+  # Sidebar Configuration
+  with st.sidebar:
+    with st.expander("⚙️ Configuration", expanded=True):
+      model_provider = st.selectbox("🔌 Model Provider", ["Select a model provider"] + list(MODEL_OPTIONS.keys()), index=0, key="model_provider")
+
+      if model_provider == "Select a model provider":
+        return
 
 if __name__ == "__main__":
   main()
